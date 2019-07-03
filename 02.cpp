a114d5a25d017cc17cda4242b4d85a9815a408ed
aldylumempouw51017023 <48673578+aldylumempouw@users.noreply.github.com>
2019-07-03 22:52:49 +0800

    Add files via upload

---
+++ 02.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+#include <conio.h>
+void main ()
+{
+strukruck data_tannggal
+{
+int tanggal;
+int bulan;
+int tahun;
+};
+data_tanggal tanggal_lahir;
+tanggal_lahir.tangal = 1;
+tanggal_lahir.bulan  = 9;
+tanggal_lahir.tahun  = 1979;
+cout << tanggal_lahr.tanggal <<"<< tanggal_lahir.bulan <<
+,,/``<< tanggal_lahir.tahun;
+}
+
